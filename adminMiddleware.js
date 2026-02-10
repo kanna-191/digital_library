98f5ab1a63e34664303f0afd3c6050aa45e5f89d
kanna-191 <25B21IT012@adityauniversity.in>
2026-02-10 21:52:00 +0530

    Add admin middleware for user role verification

---
+++ adminMiddleware.js
@@ -0,0 +1,13 @@
+const User = require("../models/User");
+
+const adminMiddleware = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  if (user.role !== "admin") {
+    return res.status(403).json({ message: "Admin only" });
+  }
+
+  next();
+};
+
+module.exports = adminMiddleware;
